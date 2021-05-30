d58e84ac519d6c4b264198c86fa497be663d76f2
louise hill <louisehilldesigner@gmail.com>
2021-05-30 13:36:03 +0100

    index complete

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    article = Article.new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  resources :articles
+
+  # # R
+  # # all articles
+  # get '/articles', to: 'articles#index'
+  # # 1 article
+  # get '/articles/:id', to: 'articles#show'
+
+  # # C
+  # # form
+  # get '/articles/new', to: 'articles#new'
+  # # create
+  # post '/articles', to: 'articles#create'
+
+  # # U
+  # # form
+  # get 'articles/:id/edit', to: 'articles#edit'
+
+  # # update
+  # patch 'articles/:id', to: 'articles#update'
+
+  # # D
+  # delete 'articles/:id', to: 'articles#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
